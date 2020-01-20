c98879ab6bb55669d28f30e3c4df3c0e960192f6
DJ <dj@walker.dev>
2020-01-20 10:47:16 -0500

    bootstrap tina

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,24 @@ import { MDXProvider } from '@mdx-js/react';
 import components from '../utils/mdx-components';
 import App from 'next/app';
 import React from 'react';
+import { Tina, TinaCMS } from 'tinacms';
+import { GitClient } from '@tinacms/git-client';
 
 export default class MyApp extends App {
+  constructor() {
+    super();
+    this.cms = new TinaCMS();
+    const client = new GitClient('http://localhost:3000/___tina');
+    this.cms.registerApi('git', client);
+  }
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <MDXProvider components={components}>
-        <Component {...pageProps} />
-      </MDXProvider>
+      <Tina cms={this.cms}>
+        <MDXProvider components={components}>
+          <Component {...pageProps} />
+        </MDXProvider>
+      </Tina>
     );
   }
 }
